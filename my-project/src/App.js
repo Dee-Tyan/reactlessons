286ee0a9c8712682bfbd66da6b9aaf2830124047
Dee-Tyan <debbietiyan@gmail.com>
2022-09-30 02:11:11 +0100

    environment setup with tailwind css

---
+++ my-project/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <p className="text-2xl"> This is a large text</p>
+    </div>
+  );
+}
+
+export default App;
